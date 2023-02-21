b223914085d68977a57926e58e3ec3c026b1a183
Denys-Kondratenko <dk132652@gmail.com>
2023-02-21 20:15:27 +0200

    Зробив друге завдання

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,27 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+// 1) Отримуємо посилання на ul.gallery
+const gallery = document.querySelector(".gallery");
+
+// 2) Створимо елементи галереї
+const markupGalleryItems = galleryItems
+  .map(
+    ({ preview, original, description }) => `
+    <a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`
+  )
+  .join("");
+
+// 3) Додамо елементи галереї до розмітки
+gallery.insertAdjacentHTML("beforeend", markupGalleryItems);
+
+// 4) Зробимо ініціалізацію бібліотеки та налаштуємо
+var lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+  scrollZoom: false,
+});
